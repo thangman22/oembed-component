0eb0464f3d55795a8f7e7319a5e8bf8028b84f33
Warat Wongmaneekit <canopybanj@gmail.com>
2018-02-21 23:13:28 +0700

    add fetch and document-register polyfill

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import vueCustomElement from 'vue-custom-element'
+import 'document-register-element/build/document-register-element'
+import 'whatwg-fetch'
 
 import Oembed from '@/components/Oembed'
 
